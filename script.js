b8b49631ae36d0eeaec3d2d53c87dac1b3f4713d
Jay Govind Kumar <govind.iq@gmail.com>
2024-11-11 23:38:03 +0530

    revision

--- script.js
+++ script.js
@@ -1,56 +1,14 @@
-let userscore = 0;
-let compscore = 0;
+let btn = document.querySelector("button");
 
-const choice = document.querySelectorAll(".option");
-const msg = document.querySelector("#msg");
-
-const gencompchoice = () => {
-  const options = ["rock", "paper", "scissor"];
-  const randIdx = Math.floor(Math.random() * 3);
-  return options[randIdx];
-};
-
-const drawgame = () => {
-  console.log("game was draw");
-  msg.innertext = "game was draw";
-};
-
-const showWinner = (userwin) => {
-  if (userwin) {
-    console.log("you win");
-    msg.innertext = "you win";
-  } else {
-    console.log("comp win");
-    msg.innertext = "comp win";
-  }
-};
-
-const playgame = (userchoice) => {
-  console.log("user choice =", userchoice);
-  // generate computer choice
-  const compchoice = gencompchoice();
-  console.log("comp choice =", compchoice);
-
-  if (userchoice === compchoice) {
-    // draw game
-    drawgame();
-  } else {
-    let userwin = true;
-    if (userchoice === "rock") {
-      userwin = compchoice === "paper" ? false : true;
-    } else if (userchoice === "paper") {
-      userwin = compchoice === "scissor" ? false : true;
-    } else {
-      userwin = compchoice === "rock" ? false : true;
-    }
-    showWinner(userwin);
-  }
-};
-
-choice.forEach((option) => {
-  console.log(option);
-  option.addEventListener("click", () => {
-    const userchoice = option.getAttribute("id");
-    playgame(userchoice);
-  });
+btn.addEventListener("click", function () {
+  let h1 = document.querySelector("h1");
+  let randomColor = getRandomColor();
+  h1.style.color = randomColor;
 });
+
+function getRandomColor() {
+  let red = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
+  return `rgb(${red}, ${green}, ${blue})`;
+}
